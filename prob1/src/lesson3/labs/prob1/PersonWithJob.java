f7eeeeb1c7bb17f90c79d78df7141ff567afd167
Musiliu Bolaji <mabolaji@mum.edu>
2019-05-29 14:36:30 -0500

    composition changes

--- prob1/src/lesson3/labs/prob1/PersonWithJob.java
+++ prob1/src/lesson3/labs/prob1/PersonWithJob.java
@@ -1,16 +1,17 @@
 package lesson3.labs.prob1;
 
-public class PersonWithJob extends Person {
+public class PersonWithJob {
 
 	private double salary;
+	private Person person;
 
 	public double getSalary() {
 		return salary;
 	}
 
 	PersonWithJob(String n, double s) {
-		super(n);
-		salary = s;
+		this.person = new Person(n);
+		this.salary = s;
 	}
 
 	@Override
@@ -20,16 +21,18 @@ public class PersonWithJob extends Person {
 		if (!(aPerson instanceof PersonWithJob))
 			return false;
 		PersonWithJob p = (PersonWithJob) aPerson;
-		boolean isEqual = this.getName().equals(p.getName()) && this.getSalary() == p.getSalary();
+		boolean isEqual = person.getName().equals(p.person.getName()) && this.getSalary() == p.getSalary();
 		return isEqual;
 	}
 
 	public static void main(String[] args) {
-		Person p1 = new PersonWithJob("Joe", 30000);
+		
+		PersonWithJob p1 = new PersonWithJob("Joe", 30000);
 		Person p2 = new Person("Joe");
+		
 		// As PersonsWithJobs, p1 should be equal to p2
-		System.out.println("p1.equals(p2)? " + p1.equals(p2));
-		System.out.println("p2.equals(p1)? " + p2.equals(p1));
+		System.out.println("p1.equals(p2)? " + p1.person.equals(p2));
+		System.out.println("p2.equals(p1)? " + p2.equals(p1.person));
 	}
 
 }
